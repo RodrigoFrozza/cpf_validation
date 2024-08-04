5ff4fefdab849dcd7b4348b4e89be7aaeb3595de
unknown <rodrigocfrozza@hotmail.com>
2024-08-04 12:19:31 -0300

    Finalizado

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 import os
 
-# 123.456.789
-# 10 - 1
-
-def cpf_validation_1(cpf):
+def cpf_validation_1(cpf): #valida o primeiro digito do CPF
     nove_digitos = cpf[:9]
-    primeiro_digito = cpf[10]
+    primeiro_digito = int(cpf[9])
     contador_regressivo = 10
     digito_1 = 0
 
@@ -14,19 +11,66 @@ def cpf_validation_1(cpf):
         contador_regressivo -= 1
     
     digito_1 = (digito_1 * 10) % 11
-    print(digito_1)
+    digito_1 = digito_1 if digito_1 <= 9 else 0
 
+    if digito_1 == primeiro_digito:
+        return digito_1
+    else:
+        return None
+        
+def cpf_validation_2(cpf): #valida o segundo digito do CPF
+    dez_digitos = cpf[:10]
+    segundo_digito = int(cpf[10])
+    contador_regressivo = 11
+    digito_2 = 0
+
+    for digito in dez_digitos:
+        digito_2 += int(digito) * contador_regressivo
+        contador_regressivo -= 1
+    
+    digito_2 = (digito_2 * 10) % 11
+    digito_2 = digito_2 if digito_2 <= 9 else 0
+
+    if digito_2 == segundo_digito:
+        return digito_2
+    else:
+        return None
 
 def main():
+    primeiro_digito = 0
+    segundo_digito = 0
 
     while True:
+        os.system('cls')
+
         cpf = input('Digite um cpf (Apenas Números): ')
 
-        if len(cpf) > 11 or len(cpf) <= 0:
-            print('CPF precisa conter 11 Digitos.')
+        if len(cpf) != 11 or not cpf.isdigit():
+            os.system('cls')
+            print('CPF precisa conter somente 11 Digitos (sem pontos e traços).')
+            input('Aperte ENTER para recomeçar...')
             continue
         else:
-            cpf_validation_1(cpf)
+            primeiro_digito = cpf_validation_1(cpf)
+
+        if primeiro_digito == None:
+            os.system('cls')
+            print('CPF Inválido')
+            input('Aperte ENTER para recomeçar...')
+            continue
+
+        segundo_digito = cpf_validation_2(cpf)
+
+        if segundo_digito == None:
+            os.system('cls')            
+            print('CPF Inválido')
+            input('Aperte ENTER para recomeçar...')
+            continue
+        
+        print(f'O CPF {cpf} é um CPF válido!')
+        break
+            
+
 
 if __name__ == '__main__':
     main()
